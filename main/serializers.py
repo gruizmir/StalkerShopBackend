98b149792e096deea0de0efa648cea99abbf69fe
Gabriel Ruiz <gabriel@destacame.cl>
2017-05-06 19:13:25 -0300

    Comentarios en ProductSerializer

--- main/serializers.py
+++ main/serializers.py
@@ -8,6 +8,12 @@ class ProductSerializer(QueryFieldsMixin, serializers.ModelSerializer):
     u"""
     Serializador de modelo Product. Por el momento, no incluye
     métodos adicionales ni serializadores a FKs.
+
+    :Authors:
+        - Gabriel Ruiz
+
+    :Last Modification:
+        - 06.05.2017
     """
 
     class Meta:
